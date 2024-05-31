3e6f636185615e50d62b4dc80f066ed12a3884ba
Minimalon <minimalonchik@gmail.com>
2024-05-31 17:54:17 +0300

    .

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import asyncio
 import json
 import time
-
 import httpx
 
 
@@ -14,17 +13,31 @@ async def _get(url, data=None):
         pool=5.0  # Таймаут на получение соединения из пула
     )
     async with httpx.AsyncClient(timeout=timeout) as client:
-        response = await client.get(url, params=data)
-        elapsed_time = time.time() - start_time
-        text = response.text
-        if response.is_success:
-            print(f"GET {url} timeout={round(elapsed_time, 2)}")
-        return response.json()
+        try:
+            response = await client.get(url, params=data)
+            elapsed_time = time.time() - start_time
+            text = response.text
+            if response.is_success:
+                print(f"GET {url} timeout={round(elapsed_time, 2)}")
+            return response.json()
+        except httpx.ConnectTimeout:
+            print(f"Connect timeout for GET {url}")
+        except httpx.ReadTimeout:
+            print(f"Read timeout for GET {url}")
+        except httpx.WriteTimeout:
+            print(f"Write timeout for GET {url}")
+        except httpx.PoolTimeout:
+            print(f"Pool timeout for GET {url}")
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error {e.response.status_code} for GET {url}")
+        except Exception as e:
+            print(f"An unexpected error occurred for GET {url}: {e}")
 
 
 def main():
     for i in range(100):
         asyncio.run(_get('http://141.101.204.58:7000/UT/hs/Groups'))
+
 
 if __name__ == '__main__':
     main()
