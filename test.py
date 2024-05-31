e6ffa5c6d25a064dd8b72980ecfe0918cc09fc7b
Minimalon <minimalonchik@gmail.com>
2024-05-31 17:45:55 +0300

    .

--- test.py
+++ test.py
@@ -2,18 +2,23 @@ import asyncio
 import json
 import time
 
-import aiohttp
+import httpx
 
 
-async def _get(url, data='None'):
+async def _get(url, data=None):
     start_time = time.time()
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, data=data) as response:
-            elapsed_time = time.time() - start_time
-            text = await response.text()
-            if response.ok:
-                print(f"GET {url} timeout={round(elapsed_time, 2)}")
-            return json.loads(text)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=data)
+        elapsed_time = time.time() - start_time
+        text = response.text
+        if response.is_success:
+            print(f"GET {url} timeout={round(elapsed_time, 2)}")
+        return response.json()
 
-for i in range(100):
-    asyncio.run(_get('http://141.101.204.58:7000/UT/hs/Groups'))
+
+def main():
+    for i in range(100):
+        asyncio.run(_get('http://141.101.204.58:7000/UT/hs/Groups'))
+
+if __name__ == '__main__':
+    main()
